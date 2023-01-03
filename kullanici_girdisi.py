fc8714eefb06672005d16890088c44e4c2dcc313
Esra404 <esradrmz571@gmail.com>
2023-01-03 16:51:40 +0300

    def

--- kullanici_girdisi.py
+++ kullanici_girdisi.py
@@ -1,7 +1,21 @@
+# def sayi_girdisi_kontrol():
+#     girdi=input("bir sayı giriniz:")
+#     if girdi.isdigit():
+#         print("tebrikler !Sayı tipi değer girdiniz....")
+#     else:
+#         print("üzgünüz. bu bir sayı tipi değişken değildir..")
+# print(sayi_girdisi_kontrol())
+
+
 def sayi_girdisi_kontrol():
-    girdi=input("bir sayı giriniz:")
-    if girdi.isdigit():
-        print("tebrikler !Sayı tipi değer girdiniz....")
+    girdi=input("bir sayı gşrşniz=")
+
+
+    while not girdi.isdigit():
+        print("üzgünüm bu bir sayı tipi değişken değildir.")
+        girdi=input("bir sayı giriniz= ") 
+
+
     else:
-        print("üzgünüz. bu bir sayı tipi değişken değildir..")
+        print("tebrikler sayı tipi değişken girdiniz.")
 print(sayi_girdisi_kontrol())
