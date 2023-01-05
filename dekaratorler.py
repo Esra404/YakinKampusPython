db777283a59f313e8fd6a1ab2563b9220903ba5d
Esra404 <esradrmz571@gmail.com>
2023-01-05 17:18:16 +0300

    decoder

---
+++ dekaratorler.py
@@ -0,0 +1,20 @@
+#ambalajlama gibi düşünebilir.
+# def deneme():
+#     print("abc")
+# print(deneme())
+# f=deneme
+# print(f())
+# def deneme():
+#     print("deneme fonksiyonu çalışıyor")
+#     def test():
+#         return "test fonksiyonu çalışıyor"
+#     print(test())
+# print(deneme())
+# def deneme():
+#     print("deneme fonksiyonu çalışıyor")
+#     def test():
+#          return "test fonksiyonu çalışıyor"
+#     return test
+# print(deneme())
+# f=deneme()
+# print(f())
