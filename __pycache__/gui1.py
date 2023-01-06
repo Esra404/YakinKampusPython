82a7f15e0442d601f25d2654a4c43c782aeb5e6c
Esra404 <esradrmz571@gmail.com>
2023-01-06 19:43:47 +0300

    gui

--- __pycache__/gui1.py
+++ __pycache__/gui1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkcalendar import DateEntry
 master=Tk()
 canvas=Canvas(master,heigh=450,width=750)
 canvas.pack()
@@ -15,7 +16,7 @@ frame_alt_sol.place(relx=0.1,rely=0.21,relwidth=0.23,relheight=0.5)
 frame_sag=Frame(master,bg='#add8e6')
 frame_alt_sol.place(relx=0.34,rely=0.21,relwidth=0.56,relheight=0.5)
 hatirlatma_tipi_etiket=Label(frame_ust,bg='#add8e6',text="hatırlatma tipi :",font="verdana  12 bold")
-hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=LEFT)
+hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=RIGHT)
 hatirlatma_tipi_opsiyon=StringVar(frame_ust)
 hatirlatma_tipi_opsiyon.set("\t")
 hatirlatma_tipi_acilir_menu=OptionMenu(
@@ -26,5 +27,10 @@ hatirlatma_tipi_acilir_menu=OptionMenu(
     "odeme"
 )
 hatirlatma_tipi_acilir_menu.pack(padx=10,pady=10,side=LEFT)
+hatirlatma_tarih_secici=DateEntry(frame_ust,width=12,background='orange',foreground='black',borderwidth=1,locale="de_DE")
+hatirlatma_tarih_secici.top_cal.overrideredirect(False)
+hatirlatma_tarih_secici.pack(padx=10,pady=10,side=RIGHT)
+hatirlatma_tipi_etiket=Label(frame_ust,bg='#add8e6',text="hatırlatma tarihi :",font="verdana  12 bold")
+hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=RIGHT)
 
 print(master.mainloop())
