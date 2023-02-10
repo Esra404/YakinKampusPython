d0de3109c4a0c20d6d036aeea3877d3e1c171e9b
Esra404 <esradrmz571@gmail.com>
2023-02-10 16:54:23 +0300

    10.02

--- __pycache__/gui1.py
+++ __pycache__/gui1.py
@@ -15,8 +15,8 @@ frame_alt_sol=Frame(master,bg='#add8e6')
 frame_alt_sol.place(relx=0.1,rely=0.21,relwidth=0.23,relheight=0.5)
 
 
-frame_sag=Frame(master,bg='#add8e6')
-frame_alt_sol.place(relx=0.34,rely=0.21,relwidth=0.56,relheight=0.5)
+frame_alt_sag=Frame(master,bg='#add8e6')
+frame_alt_sag.place(relx=0.34,rely=0.21,relwidth=0.56,relheight=0.5)
 
 hatirlatma_tipi_etiket=Label(frame_ust,bg='#add8e6',text="hatırlatma tipi :",font="verdana  12 bold")
 hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=LEFT)
@@ -33,7 +33,7 @@ hatirlatma_tipi_acilir_menu=OptionMenu(
 )
 hatirlatma_tipi_acilir_menu.pack(padx=10,pady=10,side=LEFT)
 hatirlatma_tarih_secici=DateEntry(frame_ust,width=12,background='orange',foreground='black',borderwidth=1,locale="de_DE")
-hatirlatma_tarih_secici._top_cal.overrideredirect(FALSE)
+hatirlatma_tarih_secici._top_cal.overrideredirect(False)
 hatirlatma_tarih_secici.pack(padx=10,pady=10,side=RIGHT)
 hatirlatma_tipi_etiket=Label(frame_ust,bg='#add8e6',text="hatırlatma tarihi :",font="verdana  12 bold")
 hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=RIGHT)
@@ -60,6 +60,19 @@ C3=Checkbutton(frame_alt_sol,text="aynı gün",variable=var1,onvalue=1,offvalue=
 C3.pack(anchor=NW,pady=2,padx=25)
 
 
+#PART 3
+def gonder():
+    pass
+    return
+Label(frame_alt_sag,text="HATİRLATMA MESAJI",bg='#add8e6',font="verdana 10 bold").pack(padx=10,pady=10,anchor=NW)
+metin_alani=Text(frame_alt_sag,heigh=9,width=50)
+metin_alani.tag_configure('styls',foregroud='#bfbfbf',font=('verdana',7,'bold'))
+metin_alani.pack()
+
+karsilama_metni='mesajini buraya gir'
+metin_alani.insert(END,karsilama_metni,'style')
+gonder_butonu=Button(frame_alt_sag,text='gonder',command=master.destroy)
+gonder_butonu.pack(anchor=S)
 
 
 print(master.mainloop())
