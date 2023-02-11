af8646362ce7ed37febe86053239325d190f7c6f
Esra404 <esradrmz571@gmail.com>
2023-02-12 01:14:51 +0300

    1202

--- __pycache__/gui1.py
+++ __pycache__/gui1.py
@@ -6,12 +6,12 @@ master=Tk()
 canvas=Canvas(master,heigh=450,width=750)
 canvas.pack()
 
-frame_ust=Frame(master,bg='#add8e6')
+frame_ust=Frame(master,bg='#1d3059')
 frame_ust.place(relx=0.1,rely=0.1,relwidth=0.8,relheight=0.1)
 
 
 
-frame_alt_sol=Frame(master,bg='#add8e6')
+frame_alt_sol=Frame(master,bg='#3bb4ac')
 frame_alt_sol.place(relx=0.1,rely=0.21,relwidth=0.23,relheight=0.5)
 
 
@@ -41,7 +41,7 @@ hatirlatma_tipi_etiket.pack(padx=10,pady=10,side=RIGHT)
 
 Label(frame_alt_sol,text="Hatirlatma yöntemi",bg='#add8e6',font="Verdana 10 bold").pack(padx=10,pady=10,anchor=NW)
 var=IntVar()
-R1=Radiobutton(frame_alt_sol,text="Sisteme kaydet",variable=var,value=1,bg='#add8e6',font="Verdana 10")
+R1=Radiobutton(frame_alt_sol,text="Sisteme kaydet",variable=var,value=0,bg='#add8e6',font="Verdana 10")
 R1.pack(anchor=NW,pady=5,padx=15)
 
 R2=Radiobutton(frame_alt_sol,text="E posta gönder",variable=var,value=1,bg='#add8e6',font="Verdana 10")
@@ -52,17 +52,36 @@ C1=Checkbutton(frame_alt_sol,text="bir hafta önce",variable=var1,onvalue=1,offv
 C1.pack(anchor=NW,pady=2,padx=25)
 
 var2=IntVar()
-C2=Checkbutton(frame_alt_sol,text="bir gün önce",variable=var1,onvalue=1,offvalue=0,bg='#add8e6',font="Verdana 10")
+C2=Checkbutton(frame_alt_sol,text="bir gün önce",variable=var2,onvalue=1,offvalue=0,bg='#add8e6',font="Verdana 10")
 C2.pack(anchor=NW,pady=2,padx=25)
 
 var3=IntVar()
-C3=Checkbutton(frame_alt_sol,text="aynı gün",variable=var1,onvalue=1,offvalue=0,bg='#add8e6',font="Verdana 10")
+C3=Checkbutton(frame_alt_sol,text="aynı gün",variable=var3,onvalue=1,offvalue=0,bg='#add8e6',font="Verdana 10")
 C3.pack(anchor=NW,pady=2,padx=25)
 
 
 #PART 3
+from tkinter import messagebox
 def gonder():
-    pass
+    son_mesaj=""
+    # if var.get():
+    #     if var.get()==1:
+    #         son_mesaj +="veriniz basarıyla sisteme kaydedilmişitr"
+    #         tip=hatirlatma_tipi_opsiyon() if hatirlatma_tipi_opsiyon.get() =='' else "genel"
+    #         tarih=hatirlatma_tarih_secici.get()
+    #         mesaj=metin_alani.grt("1.0","END")
+
+
+
+
+
+
+
+
+        # elif var.get() ==2:
+        #     son_mesaj +="e posta yoluyla hatirlatma size ulaşacaktır."
+
+        # messagebox.showinfo("başarılı işlem ",son_mesaj)
     return
 Label(frame_alt_sag,text="HATİRLATMA MESAJI",bg='#add8e6',font="verdana 10 bold").pack(padx=10,pady=10,anchor=NW)
 metin_alani=Text(frame_alt_sag,heigh=9,width=50)
@@ -71,7 +90,7 @@ metin_alani.pack()
 
 karsilama_metni='mesajini buraya gir'
 metin_alani.insert(END,karsilama_metni,'style')
-gonder_butonu=Button(frame_alt_sag,text='gonder',command=master.destroy)
+gonder_butonu=Button(frame_alt_sag,text='gonder',command=gonder)
 gonder_butonu.pack(anchor=S)
 
 
