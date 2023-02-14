3e37ba0bde64f952e9f2f34cd587c018bbef7d14
Esra404 <esradrmz571@gmail.com>
2023-02-14 09:29:54 +0300

    mail

--- __pycache__/e_posta_gonderimi.py
+++ __pycache__/e_posta_gonderimi.py
@@ -1,1 +1,13 @@
 import ssl
+import smtplib
+kullanici ='kibarfeyzo669@gmail.com'
+sifre='tuzak1212'
+alici=kullanici
+#baslik 'python gonderisi'
+mesaj='deneme mesajı'
+context=ssl.create_default_context()
+port=465
+host="smtp.gmail.com"
+eposta_sunucu=smtplib.SMTP_SSL(host=host,port=port,context=context)
+eposta_sunucu.login(kullanici,sifre)
+eposta_sunucu.sendmail(kullanici,alici,mesaj)
